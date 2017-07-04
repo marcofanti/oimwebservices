4f169fbbba08970e6cb226809384fa55b90bb09a
Marco Fanti <marco.a.fanti@accenture.com>
2017-07-04 17:57:23 -0400

    Deploy batch file

---
+++ SayHelloWSConnector/deploy.cmd
@@ -0,0 +1,11 @@
+ant -f C:\Oracle\Middleware\jdeveloper\bin\ant-sca-deploy.xml ^
+-DserverURL="http://host01:8001" ^
+-DsarLocation=C:\Users\Marco\Documents\oimwebservices\SayHelloWSConnector\deploy\sca_SayHelloWSConnector_rev1.2.jar ^
+-Doverwrite=true  ^
+-Duser=weblogic ^
+-Dusername=weblogic ^
+-Dpassword=Welcome1 ^
+-DforceDefault=true 
+
+
+
